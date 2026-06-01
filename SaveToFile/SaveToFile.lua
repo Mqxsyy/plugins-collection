d6932f573d171a82a66027e810572f2bd9b2e547
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2026-06-01 14:57:45 +0000

    Update SaveToFile.lua

--- SaveToFile/SaveToFile.lua
+++ SaveToFile/SaveToFile.lua
@@ -4,12 +4,12 @@
 
 --[[
 
-	Prompts to save the selected instance to a file and automatically sets it's filename.
+    Prompts to save the selected instance to a file and automatically sets it's filename.
 
-	Additionally deletes original selected object, waits for a replacement by rojo, which it
-	deletes and adds back the original.
+    Additionally deletes original selected object, waits for a replacement by rojo, which it
+    deletes and adds back the original.
 
-	This prevents rojo from corrupting the object.
+    This prevents rojo from corrupting the object.
 
 ]]
 
@@ -17,40 +17,42 @@ local Selection = game:GetService("Selection")
 
 local Toolbar = plugin:CreateToolbar("Save To File")
 local SaveButton = Toolbar:CreateButton(
-	"SaveToFile", 
-	"Save selected object to .rbxm", 
-	"rbxassetid://18961222990"
+    "SaveToFile", 
+    "Save selected object to .rbxm", 
+    "rbxassetid://99870550578082"
 )
 
+SaveButton.ClickableWhenViewportHidden = true
+
 SaveButton.Click:Connect(function()
-	local selection = Selection:Get()
+    local selection = Selection:Get()
 
-	if #selection < 1 then 
-		warn("Select an object to export")
-		return	
-	end
+    if #selection < 1 then 
+        warn("Select an object to export")
+        return    
+    end
 
-	if #selection > 1 then
-		warn("Selection includes multiple objects, please select only one")
-		return
-	end
+    for _, selectedObject in selection do
+        local copy = selectedObject:Clone()
+        local parent = selectedObject.Parent
 
-	local selectedObject = selection[1]
+        Selection:Set({ selectedObject })
+        local success = plugin:PromptSaveSelection(selectedObject.Name)
+        if success then
+            task.wait()
+            selectedObject:Destroy()
 
-	local copy = selectedObject:Clone()
-	local parent = selectedObject.Parent
+            local ghostInstance = parent:WaitForChild(copy.Name, 1) 
+            if ghostInstance then
+                ghostInstance:Destroy()
+            end
 
-	local success = plugin:PromptSaveSelection(selectedObject.Name)
-	if success then
-		task.wait()
-		selectedObject:Destroy()
-		
-		local ghostInstance = parent:WaitForChild(copy.Name, 1) 
-		if ghostInstance then
-			ghostInstance:Destroy()
-		end
-
-		task.wait()
-		copy.Parent = parent
-	end
+            task.wait()
+            copy.Parent = parent
+        end
+        
+        task.wait()
+    end    
+    
+    Selection:Set({})
 end)
